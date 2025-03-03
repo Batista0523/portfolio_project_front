003cfbbe741544f0c137b42ff664dc4a880cda26
batista0523 <elisaulbatista@pursuit.org>
2025-03-03 16:17:05 -0500

    fetch all projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import NavBar from './Components/NavBar'
 import Home from './Components/Home'
 import OneProject from './Pages/OneProject'
 import UpdateProject from './Pages/UpdateProject'
-
+import AllProjects from './Pages/AllProjects'
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
           <Route path='/' element={<Home/>} />
           <Route path='/oneProject/:id' element={<OneProject/>}/>
           <Route path='/updateProject' element={<UpdateProject/>}/>
+          <Route path='/allProjects' element={<AllProjects/>}/>
         </Routes>
        </Router>
       </div>
--- src/Pages/AllProjects.jsx
+++ src/Pages/AllProjects.jsx
@@ -1,13 +1,36 @@
 // fetch all projects and Link to navigate to One component
 
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function AllProjects() {
+  const [allProjects, setAllProjects] = useState([]);
+  const url = import.meta.env.VITE_BASE_URL;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${url}/Projects`);
+        const data = await response.json();
+        if (data.success) {
+          setAllProjects(data.payload);
+        } else {
+          console.error("Error fetchinf data", data);
+        }
+      } catch (err) {}
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      <h3>all projects</h3>
+      {allProjects.map((project, index) => (
+        <div key={index}>
+          <h1>Title : {project.title}</h1>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default AllProjects
+export default AllProjects;
