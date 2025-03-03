e9122484a8b4530717771c7bc9eb4f164b1bf9b9
batista0523 <elisaulbatista@pursuit.org>
2025-03-03 15:50:08 -0500

    added repo

---
+++ src/Pages/AllProjects.jsx
@@ -0,0 +1,13 @@
+// fetch all projects and Link to navigate to One component
+
+import React from 'react'
+
+function AllProjects() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllProjects
---
+++ src/Pages/OneProject.jsx
@@ -0,0 +1,14 @@
+// all details and delete functions plus Link to update component
+
+import React from 'react'
+
+
+function OneProject() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OneProject
